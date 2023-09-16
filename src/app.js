b1a19c457e0fdd7331c4f9bdd7a5a109afa36710
Alejandro Barraza <71998273+alejandroBarraza@users.noreply.github.com>
2023-09-16 04:26:45 +0000

    feat(app.js) challange complete

--- src/app.js
+++ src/app.js
@@ -5,7 +5,30 @@ import "./style.css";
 import "./assets/img/rigo-baby.jpg";
 import "./assets/img/4geeks.ico";
 
+function generateExcuse() {
+  let who = ["The dog", "My grandma", "His turtle", "My bird"];
+  let action = ["ate", "peed", "crushed", "broke"];
+  let what = ["my homework", "the keys", "the car"];
+  let when = [
+    "before the class",
+    "right on time",
+    "when I finished",
+    "during my lunch",
+    "while I was praying"
+  ];
+
+  const randomWho = who[Math.floor(Math.random() * who.length)];
+  const randomAction = action[Math.floor(Math.random() * action.length)];
+  const randomWhat = what[Math.floor(Math.random() * what.length)];
+  const randomWhen = when[Math.floor(Math.random() * when.length)];
+
+  const excuse = `${randomWho} ${randomAction} ${randomWhat} ${randomWhen}`;
+
+  return excuse;
+}
+
 window.onload = function() {
   //write your code here
-  console.log("Hello Rigo from the console!");
+  const paragraph = document.getElementById("excuse");
+  paragraph.innerHTML = generateExcuse();
 };
